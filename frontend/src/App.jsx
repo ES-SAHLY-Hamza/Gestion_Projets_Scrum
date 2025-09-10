d1de37b846036fc0e36a72771b959fe45d818f2b
ES-SAHLY-Hamza <h.essahly@esisa.ac.ma>
2025-09-10 19:09:19 +0100

    frontend of our application Scrum projet

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CollaboratorsList from "./componants/CollaboratorsList";
+
+function App() {
+  return (
+    <div>
+      <CollaboratorsList />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/componants/CollaboratorsList.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import "../styles/CollaboratorsList.css";
+
+const CollaboratorsList = () => {
+  const [collaborators, setCollaborators] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/collaborateurs/")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Erreur HTTP " + res.status);
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setCollaborators(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Erreur lors du fetch :", error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <p>Chargement des données...</p>;
+  }
+
+  return (
+    <div className="table-container">
+      <h1>Liste des Collaborateurs</h1>
+      <table className="styled-table">
+        <thead>
+          <tr>
+            <th>Nom</th>
+            <th>Prénom</th>
+            <th>Date d'intégration</th>
+            <th>Années d'expérience</th>
+            <th>Rôle</th>
+          </tr>
+        </thead>
+        <tbody>
+          {collaborators.map((col) => (
+            <tr key={col.id}>
+              <td>{col.nom}</td>
+              <td>{col.prenom}</td>
+              <td>{new Date(col.date_integration).toLocaleDateString()}</td>
+              <td>{col.annees_experience}</td>
+              <td>{col.role}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default CollaboratorsList;
